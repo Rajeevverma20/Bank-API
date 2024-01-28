a89d87cd49e74bbe9278b00ff7b69ddabce65fd7
Rajeevverma20 <rv208469@gmail.com>
2024-01-28 14:26:00 +0530

    Mongo DB Credentials Update

--- src/Schema/connection.js
+++ src/Schema/connection.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connect = mongoose.connect(process.env.MONGO_URL)
-                .then(()=>console.log('Database connect'))
-                .catch((err)=>console.log(err));
+const MONGO_URL = process.env.MONGO_URL;
 
+const connect = mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.error('Error connecting to database:', err));
 
 module.exports = connect;
